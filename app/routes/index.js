f71ce2180b30b17ed3d47366cddcccb90ddf6669
Rashid Machingal <rashileocontact@gmail.com>
2023-06-16 16:33:16 +0530

    result page ui

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path');
-
-// defining html file path
-const pages = path.join(__dirname, "../../pages/")
 
 // serve home page
 router.get("/", (req, res) => {
     res.render("pages/index")
+})
+
+// result page
+router.get("/english-malayalam/:word", (req, res) => {
+    res.render("pages/result")
 })
 
 module.exports = router
