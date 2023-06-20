51a023814d6d122e99894077788a8596ac522fa5
Rashid Machingal <rashileocontact@gmail.com>
2023-06-20 13:03:01 +0530

    add new word rest api created

--- app/controllers/wordController.js
+++ app/controllers/wordController.js
@@ -16,4 +16,29 @@ const getWordMeaning = async (english_word) => {
     }
 };
 
-module.exports = { getWordMeaning }
+// add new word to database
+const addNewWord = async (req, res) => {
+  try {
+    
+
+    // if the word already exists sent error message
+    const existingWord = await Word.findOne({ english_word });
+    if (existingWord) return res.status(400).json({ message: 'word already exists' });
+
+    // create a new word instance using the Word model
+    const newWord = new Word({
+      english_word,
+      meanings
+    });
+
+    // save the new word to the database and sent success message
+    const addedWord =  await newWord.save();
+    res.status(201).json({ message: 'word added successfully', word: addedWord });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'error occurred while adding the word' });
+  }
+};
+
+module.exports = { getWordMeaning, addNewWord }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getWordMeaning } = require("../controllers/wordController");
+const { getWordMeaning, addNewWord } = require("../controllers/wordController");
 const router = express.Router();
 
 // serve home page
@@ -17,6 +17,11 @@ router.get("/english-malayalam/:word", async (req, res) => {
 // serve add new word page
 router.get("/add-new-word", async (req, res) => {
     res.render("pages/add-new-word")
+})
+
+// add new word to database api
+router.post("/add-new-word", (req, res) => {
+    addNewWord(req, res)
 })
 
 module.exports = router
