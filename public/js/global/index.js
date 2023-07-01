b96271371118f66738fa8024234375098f290f87
Rashid Machingal <rashileocontact@gmail.com>
2023-07-01 23:21:33 +0530

    if user logged in hide notice in add new word

--- public/js/global/index.js
+++ public/js/global/index.js
@@ -1,15 +1,19 @@
 const accountMenu = document.querySelector(".account-menu")
 const logginButton = document.querySelector(".login")
+const noticeBoard = document.querySelector(".notice-board")
 const diplayName = document.getElementById("display_name")
 const dropdown = document.getElementById("dropdown")
 
-// if user loggedIn show account menu if not loggedIn show login button
+// if user loggedIn 
+// show account menu & hide notice in add new word page
 function showAccountMenu() {
     if(document.cookie !== ""){
         logginButton.style.display = "none"
         accountMenu.style.display = "flex"
+        noticeBoard.style.display = "none"
     }
 }
+
 
 showAccountMenu()
 
