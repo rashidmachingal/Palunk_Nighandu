61b8f7b4f372097d0cd897c4edeaf928fdf1cd36
Rashid Machingal <rashileocontact@gmail.com>
2023-07-18 19:12:21 +0530

    reject & ok contribution controllers moved to adminController.js

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -18,4 +18,68 @@ const getNewMeanings = async () => {
     }
   };
 
-module.exports = { getNewMeanings }
+  // contribution ok
+// if contribution ok remove it details from db
+const contributionOk = async (req, res) => {
+  // req.params._id for access change collection
+  // remove it from db
+  await Change.findByIdAndDelete(req.params._id)
+  res.redirect("/admin/new-meanings")
+}
+
+// reject contribution
+const rejectContributionNewMeaning = async (req, res) => {
+  try {
+    // req.params.key for => find word
+    // req.params._id for => find word meaning
+    // req.params.for_change for => access change collection
+    // req.params.user_id for access user details
+    
+    // remove contribution from word meanings array
+    await  Word.findByIdAndUpdate(req.params.key, { $pull: { meanings: { _id: req.params._id } } })
+    // remove change in change details
+    await Change.findByIdAndDelete(req.params.for_change)
+
+    // if there is user
+      if(req.params.user_id !== "no_user"){
+      // decrease user contribution count
+      const updatedWord = await Word.findOneAndUpdate(
+        {
+          _id: req.params.key,
+          'contributers.user_id': req.params.user_id
+        },
+        { $inc: { 'contributers.$.count': -1 } },
+        { new: true }
+      );
+      
+
+      // remove contributer if count === 0
+      const contributor = updatedWord.contributers.find(contributor => contributor.user_id === req.params.user_id);
+      if (contributor.count === 0) {
+        updatedWord.contributers.pull({ user_id: req.params.user_id })
+      }
+      await updatedWord.save()
+
+      // change approval status for user
+      const updateUser = await User.findOneAndUpdate(
+        {
+          _id: req.params.user_id,
+          'contributions.key': req.params._id
+        },
+        { $set: { 'contributions.$.approved': false } },
+        { new: true }
+      );
+
+      await updateUser.save();
+
+    }
+
+
+  
+    res.redirect("/admin/new-meanings")
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = { getNewMeanings, rejectContributionNewMeaning, contributionOk }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const { adminVerfication } = require("../utils/authUtils");
-const { getNewMeanings } = require("../controllers/adminController");
-const { contributionOk, rejectContribution } = require("../controllers/wordController");
+const { getNewMeanings, contributionOk, rejectContributionNewMeaning } = require("../controllers/adminController");
 const router = express.Router();
 
 // serve new-meanings page
 router.get("/new-meanings", adminVerfication, async (req, res) => {
     const newMeaningsData = await getNewMeanings()
-   // console.log(newMeaningsData)
     res.render("admin/new-meanings", { newMeaningsData })
-    //console.log(newMeaningsData,"@newMeaningsData")
+})
+
+// reject contribution => new-meanings to exsting word 
+router.post("/reject/:_id/:key/:for_change/:user_id" , adminVerfication , (req, res) => {
+    rejectContributionNewMeaning(req, res)
 })
 
 // contribution ok api 
 router.post("/contribution-ok/:_id" , adminVerfication, async (req, res) => {
     contributionOk(req, res)
-})
-
-// reject contribution 
-router.post("/reject/:_id/:key/:for_change/:user_id" , adminVerfication , (req, res) => {
-    rejectContribution(req, res)
 })
 
 module.exports = router
